6e7326aecd81b37adedf50130608c97390a594b5
YinhaoHe <yinhaoh@uci.edu>
2020-08-10 15:35:51 -0700

    Update

--- gitauto.sh
+++ gitauto.sh
@@ -1,12 +1,12 @@
-echo "git auto start..."
+echo -e "\033[32m[git auto start...]\033[0m"
 msg=$1
 if [ -n "$msg" ]; then
    git add -A
    git commit -m"${msg}"
    git pull
-   echo "add, commit, pull finished, enter password to push"
+   echo "\033[32m[add, commit, pull finished, enter password to push]\033[0m"
    git push origin master
-   echo "git auto successfully finished!"
+   echo "\033[32m[git auto successfully finished!]\033[0m"
 else
-   echo "Please add commit message e.g. (bash gitauto.sh message)" 
+   echo -e "\033[31m\033[01m\033[05m[Please add commit message e.g. (bash gitauto.sh \"commit message\")]\033[0m" 
 fi
