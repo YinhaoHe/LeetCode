522537e16fafe1925a4bfd16111ef6b2b6fa1e2e
YinhaoHe <yinhaoh@uci.edu>
2020-08-10 15:37:05 -0700

    Update

--- gitauto.sh
+++ gitauto.sh
@@ -4,9 +4,9 @@ if [ -n "$msg" ]; then
    git add -A
    git commit -m"${msg}"
    git pull
-   echo "\033[32m[add, commit, pull finished, enter password to push]\033[0m"
+   echo -e "\033[32m[add, commit, pull finished, enter password to push]\033[0m"
    git push origin master
-   echo "\033[32m[git auto successfully finished!]\033[0m"
+   echo -e "\033[32m[git auto successfully finished!]\033[0m"
 else
    echo -e "\033[31m\033[01m\033[05m[Please add commit message e.g. (bash gitauto.sh \"commit message\")]\033[0m" 
 fi
