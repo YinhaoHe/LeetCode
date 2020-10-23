8567ab33a62ec46cc060ecaf5f04ee0d1e449f9a
YinhaoHe <yinhaoh@uci.edu>
2020-10-23 16:10:14 -0700

    Leetcode 341 Flatten Nested List Iterator - Build interfaces - recursive

---
+++ 341_FlattenNestedListIterator_Build_interface.java
@@ -0,0 +1,55 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+import java.util.NoSuchElementException;
+
+public class NestedIterator implements Iterator<Integer> {
+
+    private List<Integer> integers = new ArrayList<Integer>();
+    private int position = 0;
+    
+    public NestedIterator(List<NestedInteger> nestedList) {
+        flattenList(nestedList);
+    }
+    
+    private void flattenList(List<NestedInteger> nestedList) {
+        for (NestedInteger nestedInteger : nestedList) {
+            if (nestedInteger.isInteger()) {
+                integers.add(nestedInteger.getInteger());
+            } else {
+                flattenList(nestedInteger.getList());
+            }
+        }
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) throw new NoSuchElementException();
+        return integers.get(position ++);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < integers.size();
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
