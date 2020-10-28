6a105f00f9e7a6f1add5712d2fe1125906204d9c
YinhaoHe <yinhaoh@uci.edu>
2020-10-28 12:16:23 -0700

    LeetCode 1374

---
+++ 1374_given_integer_N_odd_number_of_characters.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String generateTheString(int n) {
+        StringBuilder sb = new StringBuilder(n);
+        while (sb.length() < n - 1) {
+            sb.append('a');
+        }
+        if (n % 2 == 0) {
+            sb.append('b');
+        } else {
+            sb.append('a');
+        }
+        return sb.toString();
+    }
+}
