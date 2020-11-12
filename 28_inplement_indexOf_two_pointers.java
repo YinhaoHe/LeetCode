e16dbe027863b70f54f1def7983dd0cb631f035f
YinhaoHe <yinhaoh@uci.edu>
2020-11-12 00:43:51 -0800

    Two pointer implement indexOf

---
+++ 28_inplement_indexOf_two_pointers.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int lh = haystack.length(), ln = needle.length();
+        if (ln == 0) return 0;
+        
+        // two pointers ph pn; 
+        int ph = 0;
+        
+        while (ph < lh - ln + 1) {
+            // find the first position of ph
+            while (ph < lh - ln + 1 && haystack.charAt(ph) != needle.charAt(0)) {
+                ++ ph;
+            }
+
+            int curLength = 0, pn = 0;
+            while (ph < lh && pn < ln && haystack.charAt(ph) == needle.charAt(pn)) {
+                ++ ph;
+                ++ pn;
+                ++ curLength;
+            }
+            
+            if (curLength == ln) {
+                return ph - ln;
+            }
+            ph = ph - curLength + 1;
+        }
+        return -1;
+    }
+}
