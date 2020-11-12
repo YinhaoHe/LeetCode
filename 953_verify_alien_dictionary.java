90980fc1d46cadb6a6fc00ca780f315c58a543b6
YinhaoHe <yinhaoh@uci.edu>
2020-11-11 19:32:49 -0800

    953 nested for; label for

---
+++ 953_verify_alien_dictionary.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] index = new int[26];
+        for (int i = 0; i < order.length(); ++i) {
+            index[order.charAt(i) - 'a'] = i;
+        }
+
+        search: for (int i = 0; i < words.length - 1; ++i) {
+            String word1 = words[i];
+            String word2 = words[i + 1];
+
+            for (int k = 0; k < Math.min(word1.length(), word2.length()); ++k) {
+                if (word1.charAt(k) != word2.charAt(k)) {
+                    if (index[word1.charAt(k) - 'a'] > index[word2.charAt(k) - 'a']) {
+                        return false;
+                    } else {
+                        continue search;
+                    }
+                }
+            }
+            if (word1.length() > word2.length())
+                return false;
+        }
+        return true;
+    }
+}
