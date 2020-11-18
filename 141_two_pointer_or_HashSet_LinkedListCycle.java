d0e0255b958d603730f0a07581a4b82d06882493
YinhaoHe <yinhaoh@uci.edu>
2020-11-17 16:09:34 -0800

    Leetcode

--- 141_two_pointer_or_HashSet_LinkedListCycle.java
+++ 141_two_pointer_or_HashSet_LinkedListCycle.java
@@ -2,7 +2,7 @@
  * Definition for singly-linked list.
  * class ListNode {
  *     int val;
- *     ListNode next;
+ *     ListNode next; 
  *     ListNode(int x) {
  *         val = x;
  *         next = null;
