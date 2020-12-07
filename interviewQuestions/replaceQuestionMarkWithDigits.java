ac18ea7ad55b2e16445e7c08cd8bb9bb800c7b7e
YinhaoHe <yinhaoh@uci.edu>
2020-12-07 14:34:32 -0800

    replace question mark with digits

---
+++ interviewQuestions/replaceQuestionMarkWithDigits.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+
+	public String solution(String S) {
+
+		char[] digits = {'1', '2', '3'};
+		char[] s = S.toCharArray();
+
+		int len = S.length();
+		char left = ' ';
+		char right = ' ';
+
+		for (int i = 0; i < len; i ++) {
+			if (Character.isDigit(s[i])) continue;
+			if (i > 0) left = s[i - 1];
+			if (i < len - 1) right = s[i + 1];
+
+			for (char digit : digits) {
+				if (digit != left && digit != right) {
+					s[i] = digit;
+					break;
+				}
+			}
+		}
+		return new String(s);
+	}
+
+	public static void main(String args[]) {
+		String test = "?1???2???3???1?";
+		Solution sol = new Solution();
+		
+		System.out.println(sol.solution(test));
+	}
+}
