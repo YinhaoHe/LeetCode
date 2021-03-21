063410a79786b3c453cc741e1239249561fe01d9
YinhaoHe <yinhaoh@uci.edu>
2021-03-20 23:37:01 -0700

    Amazon interview

---
+++ Amazon/AmazonInterview/652FindDuplicateSubtrees.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private ArrayList<TreeNode> ans;
+    private HashMap<String, Integer> count;
+    
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        ans = new ArrayList<>();
+        count = new HashMap<>();
+        collect(root);
+        return ans;
+    }
+    
+    public String collect(TreeNode node) {
+        if (node == null) return "#";
+        String serial = node.val + "," + collect(node.left) + "," + collect(node.right);
+        count.put(serial, count.getOrDefault(serial, 0) + 1);
+        if (count.get(serial) == 2) 
+            ans.add(node);
+        return serial;
+    }
+}
