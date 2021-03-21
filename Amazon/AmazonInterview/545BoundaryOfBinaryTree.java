a49b0c7e285bf23f122235283ceb10783586773f
YinhaoHe <yinhaoh@uci.edu>
2021-03-21 15:03:01 -0700

    Amazon interview questions

---
+++ Amazon/AmazonInterview/545BoundaryOfBinaryTree.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public boolean isLeaf (TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+    
+    public void addLeaves(List<Integer> res, TreeNode root) {
+        if (isLeaf(root)) {
+            res.add(root.val);
+        } else {
+            if (root.left != null) {
+                addLeaves(res, root.left);
+            }
+            if (root.right != null) {
+                addLeaves(res, root.right);
+            }
+        }
+    }
+    
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        if (!isLeaf(root)) {
+            res.add(root.val);
+        }
+        TreeNode t = root.left;
+        while (t != null) {
+            if (!isLeaf(t)) {
+                res.add(t.val);
+            } 
+            if (t.left != null) {
+                t = t.left;
+            } else {
+                t = t.right;
+            }
+        }
+        addLeaves(res, root);
+        Stack<Integer> s = new Stack<>();
+        t = root.right;
+        while (t != null) {
+            if (!isLeaf(t)) {
+                s.push(t.val);
+            }
+            if (t.right != null) {
+                t = t.right;
+            } else {
+                t = t.left;
+            }
+        }
+        while (!s.isEmpty()) {
+            res.add(s.pop());
+        }
+        return res;
+    }
+}
